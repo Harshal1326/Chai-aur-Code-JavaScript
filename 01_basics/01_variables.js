3d17c00e4e403edfc33b2fd2dd256167fece4e20
Harshal Shinde <162967827+Harshal1326@users.noreply.github.com>
2026-02-18 14:42:09 +0000

    01_variables commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144533
+let accountEmail = "harshal@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
