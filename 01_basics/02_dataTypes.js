129bd195a0f22185609249203623d682aa016791
Harshal Shinde <162967827+Harshal1326@users.noreply.github.com>
2026-02-19 12:37:13 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node.js here not brower so it will not work
+
+// code should have high readability
+
+console.log("Harshal")
+
+let name = "Harshal" // String Data Type
+let age = 18  // int
+let isLogedIn = false // boolean
+
+// Data Types list => number -> 2 to power 53, bigint, String, boolean, null -> standalone value, undefined, symbol -> to find unique
+
+// Object 
+
+console.log(typeof null);  // o/p: object
+console.log(typeof undefined);  // o/p: undefined
