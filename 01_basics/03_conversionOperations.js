8b329290fd06b141bcf7c2fa5412dfb918a6ebdc
Harshal Shinde <162967827+Harshal1326@users.noreply.github.com>
2026-02-19 14:35:03 +0000

    changes in this

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -30,3 +30,35 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+// ------- OPERATIONS --------- //
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " harshal"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log((3 + 4) * 5 % 3);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
